b660ff576228a97596c8126432df4a997bb90e2d
RachaelSarah <142069496+RachaelSarah@users.noreply.github.com>
2025-03-05 12:14:43 +0300

    Database connection

--- connectdb.php
+++ connectdb.php
@@ -7,7 +7,7 @@ $dbname = "summer21"; // your database name
 // Create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check connection.
 if ($connect->connect_error) {
     die("Connection failed: " . $connect->connect_error);
 } else {
